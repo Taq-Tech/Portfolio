5a23669c83aa66b4d6d3337fd4a92d3bebdc46ed
Lana Touma <LanaTouma.s@gmail.com>
2026-04-21 13:21:14 +0300

    responsivness

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.css',
 })
 export class Navbar {
+  menuOpen = signal(false);
 
+  toggleMenu() {
+    this.menuOpen.update(v => !v);
+  }
 }
